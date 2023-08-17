dfbd0119c92734fda1806b8104323c5baaebbe89
Mohammad Reza <mo.behzad34@gmail.com>
2023-08-17 11:28:11 +0330

    Initial Commit

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by Mohammad Behzadfar on 5/26/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
